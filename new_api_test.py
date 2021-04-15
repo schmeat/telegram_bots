5ae70e8357e20be8c0d2db34731daac2a7c08d02
Ajaykumar Kannan <kannan.ajay@gmail.com>
2021-04-15 16:48:04 +0000

    Playing with the new API

--- new_api_test.py
+++ new_api_test.py
@@ -4,8 +4,12 @@
 import COVID19Py
 for source in ["jhu", "csbs", "nyt"]:
     print(source)
-    covid19 = COVID19Py.COVID19(data_source=source)
-    locations = covid19.getLocations()
+    locations=[]
+    try:
+        covid19 = COVID19Py.COVID19(data_source=source)
+        locations = covid19.getLocations()
+    except:
+        print("Houston, we have a problem.")
     # print(locations)
     last_country = ""
     # print(locations[0])
